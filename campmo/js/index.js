fcf00f475eb1c36095b67434b82a6ade7250cfd2
Faizanv <fvirani6@gatech.edu>
2015-04-22 20:02:10 -0400

    Fully functional webapp yo

--- campmo/js/index.js
+++ campmo/js/index.js
@@ -1,47 +1,58 @@
 $(document).ready(function() {
-  $("#send").click(function() {
-    send()
-  });
+    var fb = new Firebase('https://camp-mosaic.firebaseio.com/');
 
-  $("input").keypress(function(event) {
-      if (event.which == 13) {
-          event.preventDefault();
-          $("#send").click();
-      }
-  });
+    $("#send").click(function() {
+        send()
+    });
 
+    $("input").keypress(function(event) {
+        if (event.which == 13) {
+            event.preventDefault();
+            $("#send").click();
+        }
+    });
 
-
-  function send() {
-    var message = $("#msg").val();
-    if (message.trim()) {
-        $("#msg").val('');
-        var submit = {
-            'where': {
-                'appName': 'CampMo'
-            },
-            'data': {
-                'alert': message
-            }
-        };
-        submit = JSON.stringify(submit);
-        console.log(submit);
-        $.ajax({
-            url: 'https://api.parse.com/1/push',
-            headers: {
-                "X-Parse-Application-Id": "PkhBXQHKFDqNVNgsnYrNYVLOiFfdWkPpVCx1173K",
-                "X-Parse-REST-API-Key": "lbncgY1nEvxvLAI41rLqE8YmKvT37wCIyPOznPxb",
-                "Content-Type": "application/json"
-            },
-            data: submit,
-            error: function(e) {
-                console.log(e);
-            },
-            success: function(data) {
-                console.log("Notification sent");
-            },
-            type: 'POST'
-        });
+    function send() {
+        var message = $("#msg").val();
+        var description = $("#description").val();
+        fb.push({sender: "admin", msg: message, detail: description});
+        if (message.trim()) {
+            $("#msg").val('');
+            $("#description").val('');
+            var submit = {
+                'where': {
+                    'appName': 'CampMo'
+                },
+                'data': {
+                    'alert': message
+                }
+            };
+            submit = JSON.stringify(submit);
+            console.log(submit);
+            $.ajax({
+                url: 'https://api.parse.com/1/push',
+                headers: {
+                    "X-Parse-Application-Id": "PkhBXQHKFDqNVNgsnYrNYVLOiFfdWkPpVCx1173K",
+                    "X-Parse-REST-API-Key": "lbncgY1nEvxvLAI41rLqE8YmKvT37wCIyPOznPxb",
+                    "Content-Type": "application/json"
+                },
+                data: submit,
+                error: function(e) {
+                    console.log(e);
+                },
+                success: function(data) {
+                    console.log("Notification sent");
+                },
+                type: 'POST'
+            });
+        }
     }
-}
+
+
+    fb.on('child_added', function(snapshot) {
+        var snap = snapshot.val();
+        console.log(snap.msg);
+        $("#notif").append("<div class='message'><strong>Message: </strong>" + snap.msg + "</br><strong>Detail: </strong>" + snap.detail + "</div>");
+    });
+
 });
