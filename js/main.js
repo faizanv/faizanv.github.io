50f6979036a859157bb6ed1f7188a7d9e6c075ee
faizanv <faizan.virani.44@gmail.com>
2017-08-30 01:53:55 -0700

    Updated blinking text and changed image

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
   var des = [
+    "I can write code",
     "I can follow documentation",
-    "I work at <span class='yellow-text text-darken-1'>Snap Inc.</span>",
-    "I go to <span class='yellow-text text-darken-1'>G</span>eorgia <span class='yellow-text text-darken-1'>T</span>ech",
+    "I work at <span class='orange-text'>Amazon</span>",
+    "I go to <span class='yellow-text text-darken-2'>G</span>eorgia <span class='yellow-text text-darken-2'>T</span>ech",
+    "I worked at <span class='yellow-text text-darken-2'>Snap Inc.</span>",
     "I direct <span class='teal-text'>HackGT</span>",
     "I worked at <span class='teal-text'>SpaceX</span>",
     "I can write code",
     "I can't design",
-    "I'm a <span class='yellow-text text-darken-1'>Yellow</span> Jacket",
+    "I'm a <span class='yellow-text text-darken-2'>Yellow</span> Jacket",
     "I can <span class='teal-text'>node.js</span>",
   ];
     var i = 0;
