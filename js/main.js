11b88939f2fb4678e60ea89f4ab18a521be5a63b
Virani <fvvirani@amazon.com>
2017-09-16 22:33:43 -0700

    Edited description list

--- js/main.js
+++ js/main.js
@@ -1,17 +1,15 @@
 $(document).ready(function() {
   $(".button-collapse").sideNav();
   var des = [
-    "I can write code",
-    "I can follow documentation",
-    "I work at <span class='orange-text'>Amazon</span>",
-    "I go to <span class='yellow-text text-darken-2'>G</span>eorgia <span class='yellow-text text-darken-2'>T</span>ech",
-    "I worked at <span class='yellow-text text-darken-2'>Snap Inc.</span>",
-    "I direct <span class='teal-text'>HackGT</span>",
-    "I worked at <span class='teal-text'>SpaceX</span>",
-    "I can write code",
-    "I can't design",
-    "I'm a <span class='yellow-text text-darken-2'>Yellow</span> Jacket",
-    "I can <span class='teal-text'>node.js</span>",
+    "Intern at <span class='orange-text'>Amazon</span> Fall 2017",
+    "<span class='yellow-text text-darken-2'>G</span>eorgia <span class='yellow-text text-darken-2'>T</span>ech Class of 2018",
+    "<span class='yellow-text text-darken-2'>Snap Inc.</span> Summer 2017",
+    "<span class='teal-text'>HackGT</span> Director",
+    "Intern at <span class='teal-text'>SpaceX</span> Summer 2016",
+    "Web Developer",
+    "Builder",
+    "<span class='purple-text text-darken-2'>Microsoft Imagine Cup</span> Finalist",
+    "<span class='teal-text'>Node.js</span>",
   ];
     var i = 0;
     (function runIt() {
