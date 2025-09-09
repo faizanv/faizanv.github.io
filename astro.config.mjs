afceeb593b0c20375191b271a37e5e454842f1e7
faizanv <faizan.virani.44@gmail.com>
2025-09-09 03:29:03 -0400

    Move to new astro site

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    vite: {
+        server: {
+            allowedHosts: ['bdvgd-2600-1700-a1d0-3af0-505f-b6fb-eb74-921f.a.free.pinggy.link']
+        }
+    }
+});
