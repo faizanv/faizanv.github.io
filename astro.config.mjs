14f9211e4433ac9deafe394c904b952b07e882a8
faizanv <faizan.virani.44@gmail.com>
2025-09-09 03:31:35 -0400

    github pages deploy steps?

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    vite: {
-        server: {
-            allowedHosts: ['bdvgd-2600-1700-a1d0-3af0-505f-b6fb-eb74-921f.a.free.pinggy.link']
-        }
-    }
-});
+    site: 'https://faizanv.com',
+})
