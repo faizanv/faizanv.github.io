5ba0f3121056855dc4389f676d19105f6f3b6a15
faizanv <faizan.virani.44@gmail.com>
2026-01-08 15:44:46 -0500

    Add resume redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,8 @@ export default defineConfig({
     site: 'https://faizanv.com',
     server: {
         allowedHosts: ['vjdqg-2600-1700-a1d0-3af0-c020-7d9c-cf15-d2fd.a.free.pinggy.link']
+    },
+    redirects: {
+        '/resume': '/FaizanViraniResume.pdf'
     }
 })
